1d87e90a0bcf1b4242460bdd87e90e334be72c89
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-12-23 22:47:24 +0530

    footer done

--- blogProject/src/components/Footer/Footer.jsx
+++ blogProject/src/components/Footer/Footer.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="w-full h-auto">
-      <div className="bg-gray-300 rounded-xl border">
+      <div className="bg-gray-600 rounded-xl border">
         <div className="flex flex-col">
-          <p className="text-gray-800 text-center font-medium mt-3 ">
+          <p className="text-gray-300 text-center font-medium mt-3 ">
             Hi, i'm Preeti - learning and writing about code.
           </p>
           <div className="flex justify-around">
-            <div className="text-gray-700 text-center font-medium my-10">
+            <div className="text-gray-400 text-center font-medium my-10">
               <p>&copy; 2025 All Rights Reserved.</p>
             </div>
 
             <div className="my-1 flex flex-col mt-4">
-              <p className="text-gray-500 font-bold">CONNECT</p>
-              <ul className="my-2 ml-4 text-left pl-2 list-disc">
+              <p className="text-gray-400 font-bold">CONNECT</p>
+              <ul className="my-2 ml-4 text-left pl-2 list-disc text-gray-900">
                 <li className=" my-1">
                   <Link to={"https://github.com/palpreeti23"}>Github</Link>
                 </li>
@@ -32,9 +32,9 @@ function Footer() {
               </ul>
             </div>
 
-            <div className="my-1 flex flex-col mt-4">
-              <p className="text-gray-500 font-bold">TECH</p>
-              <ul className="my-2 ml-4 text-left pl-2 list-disc">
+            <div className="my-1 flex flex-col mt-4 ">
+              <p className="text-gray-400 font-bold">TECH</p>
+              <ul className="my-2 ml-4 text-left pl-2 list-disc text-gray-900">
                 <li className="my-1">React</li>
                 <li className="my-1">Tailwind</li>
                 <li className="my-1">Appwrite</li>
