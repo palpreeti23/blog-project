43e6537a5b3dec330a1fd3da138533b253c1c42f
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-12-27 23:07:19 +0530

    update: style

--- blogProject/src/components/PostCard.jsx
+++ blogProject/src/components/PostCard.jsx
@@ -2,44 +2,43 @@ import React, { useState } from "react";
 import images from "../img/ChatGPT Image Dec 23, 2025, 11_58_52 PM.png";
 import { Link } from "react-router-dom";
 import appwriteService from "../appwrite/post";
+import parse from "html-react-parser";
 
-function PostCard({ title, featureImage, $id, content }) {
-  const images = featureImage
-    ? appwriteService.getFilePreview(featureImage)
+function PostCard({ title, featuredImage, $id, content }) {
+  const images = featuredImage
+    ? appwriteService.getFilePreview(featuredImage)
     : null;
 
   const [expand, setExpand] = useState(false);
 
   return (
-    <Link to={`/post/${$id}`}>
-      <div className="w-1/3 h-auto rounded-xl border bg-gray-200 shadow-gray-600 shadow-2xl">
-        <div className="flex flex-col ">
+    <div className="w-full h-auto rounded-xl border bg-gray-200 shadow-gray-600 shadow-2xl my-3 ">
+      <div className="flex flex-col ">
+        <Link to={`/post/${$id}`}>
           <img
-            className="w-full h-40 object-cover rounded-xl"
+            className="w-50 h-40 object-cover rounded-xl"
             src={images}
             alt="placeholder"
           />
-          <h2 className="text-gray-900 text-xl text-left px-3 py-2 ">
-            {title}
-          </h2>
-          <div>
-            <p
-              className={`text-gray-800 text-left px-3 py-1 ${
-                expand ? "" : "line-clamp-3"
-              }`}
-            >
-              {content}
-            </p>
-            <button
-              className=" text-sm text-blue-400 hover:underline pb-2 cursor-pointer block px-3 "
-              onClick={() => setExpand(!expand)}
-            >
-              {expand ? "Read less" : "Read more"}
-            </button>
-          </div>
+        </Link>
+        <h2 className="text-gray-900 text-xl text-left px-3 py-1 ">{title}</h2>
+        <div>
+          <p
+            className={`text-gray-800 text-left px-3 ${
+              expand ? "" : "line-clamp-3"
+            }`}
+          >
+            {parse(content)}
+          </p>
+          <button
+            className=" text-sm text-blue-400 hover:underline pb-2 cursor-pointer block px-3 "
+            onClick={() => setExpand(!expand)}
+          >
+            {expand ? "Read less" : "Read more"}
+          </button>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
